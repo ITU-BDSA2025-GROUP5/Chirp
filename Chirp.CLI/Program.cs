c0f625b70dabf10117f89044809397988287ce92
Oscar <osja@itu.dk>
2025-08-29 12:01:49 +0200

    ChirpV0.0.0.0.1, hardcoded version

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+    {
+        String path = "/Users/oscardalsgaardjakobsen/Documents/GitHub/Chirp/Chirp.CLI/chirp_cli_db.csv";
+        string[] lines = File.ReadAllLines(path);
+        String Author, Message, Timestamp;
+        
+        for (int i = 1; i < 5; i++)
+        {
+            string line = lines[i];
+            string[] newline = line.Split('"');
+            Message = newline[1];
+            string[] newline2 = line.Split(',');
+            Author = newline2[0];
+            Timestamp = newline2[newline2.Length - 1];
+
+            long unixSeconds = long.Parse(Timestamp);
+            DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(unixSeconds);
+            string formatted = dto.ToLocalTime().ToString("MM/dd/yy HH:mm:ss");
+            Timestamp = formatted;
+
+            Console.WriteLine(Author + " " + "@ " + formatted + ": " + Message);
+        }
+    }
