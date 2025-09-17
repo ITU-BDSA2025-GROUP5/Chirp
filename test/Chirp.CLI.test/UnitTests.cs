2ec1aeffcdb2f52f0252e6c2526d4d5407ab371c
Martin Jaeger <Martin.Jaeger@live.dk>
2025-09-17 15:58:44 +0200

    added the test.sln to the Chirp.CLI.sln in chirp folder

--- test/Chirp.CLI.test/UnitTests.cs
+++ test/Chirp.CLI.test/UnitTests.cs
@@ -1,12 +1,14 @@
+using Chirp.CLI;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 
 namespace test;
 
 public class UnitTests
 {
     [Fact]
-    public void Test1()
-    { 
-        Program.read
+    public void reads_correctly()
+    {
+
     }
 }
