65340be96ee7fde8e918648011001b82d6ae5f32
Oscar <oscarjakobsen@icloud.com>
2025-09-19 10:18:40 +0200

    Tweaks and stuff for the app

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -5,11 +5,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
 IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
 
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/cheeps", () => UserInterface.PrintCheeps(database.Read()));
+app.MapPost("/cheeps", (string message) =>
+{
+    string author = Environment.UserName;
+    var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    Cheep cheep = new Cheep { Author = author, Message = message, Timestamp = timestamp };
+    database.Store(cheep);
+});
 app.Run();
