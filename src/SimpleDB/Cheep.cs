f626ac3897638d5d0003a0cf29ad733a920160a5
Martin Jaeger <Martin.Jaeger@live.dk>
2025-09-24 14:38:37 +0200

    Removed all files and added dotnet razor

--- src/SimpleDB/Cheep.cs
+++
@@ -1,8 +0,0 @@
-namespace Chirp.CLI;
-
-public record Cheep
-{
-    public required string Author { get; set; }
-    public required string Message { get; set; }
-    public required long Timestamp { get; set; }
-}
