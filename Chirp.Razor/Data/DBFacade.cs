3aa7cda003bdd100d9eb72c94c659e40ab114edd
Oscar <oscarjakobsen@icloud.com>
2025-10-01 14:59:25 +0200

    Start of testing

--- Chirp.Razor/Data/DBFacade.cs
+++ Chirp.Razor/Data/DBFacade.cs
@@ -1,13 +1,14 @@
+using System.Data.Common;
 using Microsoft.Data.Sqlite;
 namespace Chirp.Razor.Data;
 
 public sealed class DBFacade : IDbFacade
 {
+    
     public int GetCheepCount()
     {
         var sqlDBFilePath = "/tmp/chirp.db";
         var sqlQuery = @"SELECT COUNT(*) FROM message;";
-
         using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
         {
             connection.Open();
@@ -31,8 +32,8 @@ public sealed class DBFacade : IDbFacade
         var sqlDBFilePath = "/tmp/chirp.db";
         var sqlQuery = @"
         SELECT u.username AS author,
-               m.text     AS message,
-               CAST(m.pub_date AS REAL) AS ts
+                m.text     AS message,
+                CAST(m.pub_date AS REAL) AS ts
                 FROM message m
                 JOIN user   u ON u.user_id = m.author_id
                 ORDER BY COALESCE(m.pub_date, 0) DESC
@@ -68,7 +69,7 @@ public sealed class DBFacade : IDbFacade
                 JOIN user   u ON u.user_id = m.author_id
                 WHERE u.username = $author
                 ORDER BY COALESCE(m.pub_date, 0) DESC
-                LIMIT 50;";
+                LIMIT 32;";
         var list = new List<CheepViewModel>();
         using var connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
         connection.Open();
@@ -85,6 +86,41 @@ public sealed class DBFacade : IDbFacade
             list.Add(new CheepViewModel(a, m, UnixTimeStampToDateTimeString(timestamp)));
         }
         return list;
+    }
+
+    public List<CheepViewModel> GetCheepsPage(int pageNumber, int pageSize)
+    {
+        var sqlDBFilePath = "/tmp/chirp.db";
+        var offset = (pageNumber - 1) * pageSize;
+        var sqlQuery = @"
+        SELECT  u.username AS author,
+                m.text     AS message,
+                CAST(m.pub_date AS REAL) AS ts
+                FROM message m
+                JOIN user u ON u.user_id = m.author_id
+                ORDER BY COALESCE(m.pub_date, 0) DESC
+                LIMIT $pageSize OFFSET $offset;";
+        var cheeps = new List<CheepViewModel>();
+        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = sqlQuery;
+            command.Parameters.AddWithValue("$pageSize", pageSize);
+            command.Parameters.AddWithValue("$offset", offset);
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var author = reader.GetString(0);
+                var message = reader.GetString(1);
+                var timestamp = reader.GetDouble(2);
+                var timestampString = UnixTimeStampToDateTimeString(timestamp);
+                cheeps.Add(new CheepViewModel(author, message, timestampString));
+            }
+        }
+        return cheeps;
     }
 
 
