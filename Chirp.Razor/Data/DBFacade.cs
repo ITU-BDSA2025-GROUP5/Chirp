a41318e0cb9e242340d0fb103f4219655bb61789
Martin Jaeger <Maraj@itu.dk>
2025-10-08 15:24:56 +0200

    Made try catch blocks for DBFacade methods
    This has been done so we cant test the deployment to azure without connection to db
    
    Co-authored-by: Carl <ctfh@itu.dk>
    
    Co-authored-by: Oscar <osja@itu.dk>

--- Chirp.Razor/Data/DBFacade.cs
+++ Chirp.Razor/Data/DBFacade.cs
@@ -69,22 +69,31 @@ public sealed class DBFacade : IDbFacade
     {
         var sqlDBFilePath = "/tmp/chirp.db";
         var sqlQuery = @"SELECT COUNT(*) FROM message;";
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        try
         {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
             {
-                var count = reader.GetInt32(0);
-                Console.WriteLine($"Message count: {count}");
-                return count;
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = sqlQuery;
+
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var count = reader.GetInt32(0);
+                    Console.WriteLine($"Message count: {count}");
+                    return count;
+                }
+
+                return 1;
             }
         }
-        return 0;
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return 1;
+        }
     }
 
     public List<CheepViewModel> GetCheeps()
@@ -98,25 +107,37 @@ public sealed class DBFacade : IDbFacade
                 JOIN user   u ON u.user_id = m.author_id
                 ORDER BY COALESCE(m.pub_date, 0) DESC";
         var cheeps = new List<CheepViewModel>();
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        try
         {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
             {
-                var author = reader.GetString(0);
-                var message = reader.GetString(1);
-                var timestamp = reader.GetDouble(2);
-                var timestampString = UnixTimeStampToDateTimeString(timestamp);
-                cheeps.Add(new CheepViewModel(author, message, timestampString));
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = sqlQuery;
+
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var author = reader.GetString(0);
+                    var message = reader.GetString(1);
+                    var timestamp = reader.GetDouble(2);
+                    var timestampString = UnixTimeStampToDateTimeString(timestamp);
+                    cheeps.Add(new CheepViewModel(author, message, timestampString));
+                }
+                return cheeps;
             }
         }
-        return cheeps;
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            var emptyList = new List<CheepViewModel>();
+            return emptyList;
+        }
     }
+    
+    
+    
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
     {
         var sqlDBFilePath = "/tmp/chirp.db";
@@ -130,21 +151,32 @@ public sealed class DBFacade : IDbFacade
                 ORDER BY COALESCE(m.pub_date, 0) DESC
                 LIMIT 32;";
         var list = new List<CheepViewModel>();
-        using var connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
-        connection.Open();
-        using var cmd = connection.CreateCommand();
-        cmd.CommandText = sqlQuery;
-        cmd.Parameters.AddWithValue("$author", author);
 
-        using var reader = cmd.ExecuteReader();
-        while (reader.Read())
+        try
         {
-            var a = reader.GetString(0);
-            var m = reader.GetString(1);
-            var timestamp = reader.GetDouble(2);
-            list.Add(new CheepViewModel(a, m, UnixTimeStampToDateTimeString(timestamp)));
+            using var connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
+            connection.Open();
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = sqlQuery;
+            cmd.Parameters.AddWithValue("$author", author);
+
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var a = reader.GetString(0);
+                var m = reader.GetString(1);
+                var timestamp = reader.GetDouble(2);
+                list.Add(new CheepViewModel(a, m, UnixTimeStampToDateTimeString(timestamp)));
+            }
+
+            return list;
         }
-        return list;
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            var emptyList = new List<CheepViewModel>();
+            return emptyList;
+        }
     }
 
     public List<CheepViewModel> GetCheepsPage(int pageNumber, int pageSize)
@@ -160,26 +192,36 @@ public sealed class DBFacade : IDbFacade
                 ORDER BY COALESCE(m.pub_date, 0) DESC
                 LIMIT $pageSize OFFSET $offset;";
         var cheeps = new List<CheepViewModel>();
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+
+        try
         {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-            command.Parameters.AddWithValue("$pageSize", pageSize);
-            command.Parameters.AddWithValue("$offset", offset);
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
             {
-                var author = reader.GetString(0);
-                var message = reader.GetString(1);
-                var timestamp = reader.GetDouble(2);
-                var timestampString = UnixTimeStampToDateTimeString(timestamp);
-                cheeps.Add(new CheepViewModel(author, message, timestampString));
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = sqlQuery;
+                command.Parameters.AddWithValue("$pageSize", pageSize);
+                command.Parameters.AddWithValue("$offset", offset);
+
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var author = reader.GetString(0);
+                    var message = reader.GetString(1);
+                    var timestamp = reader.GetDouble(2);
+                    var timestampString = UnixTimeStampToDateTimeString(timestamp);
+                    cheeps.Add(new CheepViewModel(author, message, timestampString));
+                }
+                return cheeps;
             }
         }
-        return cheeps;
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            List<CheepViewModel> emptyList = new List<CheepViewModel>();
+            return emptyList;
+        }
     }
 
 
