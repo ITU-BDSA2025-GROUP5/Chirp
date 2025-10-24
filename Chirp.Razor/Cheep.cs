f8a41abc2730c1804d6265bd4362752acf164945
Martin Jæger <Maraj@itu.dk>
2025-10-24 12:38:18 +0200

    constrained cheeps to max 160 characters

--- Chirp.Razor/Cheep.cs
+++ Chirp.Razor/Cheep.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Razor;
 
 public class Cheep
 {
     public int CheepId { get; set; }
-    public required int UserId { get; set; }
+    public required int UserId { get; set; } 
+    
+    [StringLength(160)]
     public required string Text { get; set; }
+    
     public required User User { get; set; }
     public DateTime TimeStamp { get; set; }
 }
