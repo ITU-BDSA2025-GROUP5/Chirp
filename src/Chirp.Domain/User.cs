e88dc753bdd39ffa0e9dcf03f2071565d9994562
Carl <cfth@itu.dk>
2025-11-21 11:09:21 +0100

    fixed login exception

--- src/Chirp.Domain/User.cs
+++ src/Chirp.Domain/User.cs
@@ -3,7 +3,7 @@ namespace Chirp.Domain;
 
 public class User : Microsoft.AspNetCore.Identity.IdentityUser
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public required ICollection<Cheep> Cheeps { get; set; }
     
 }
--- src/Chirp.Infrastructure/Migrations/20251121100617_trying to fix issue 2.Designer.cs
+++ src/Chirp.Infrastructure/Migrations/20251121100617_trying to fix issue 2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Chirp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Infrastructure.Migrations
 {
     [DbContext(typeof(CheepDbContext))]
-    partial class CheepDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121100617_trying to fix issue 2")]
+    partial class tryingtofixissue2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
