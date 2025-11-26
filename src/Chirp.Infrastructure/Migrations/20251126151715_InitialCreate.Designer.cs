0aa48a1ffa0d733d1384d8434ff6738a27839ed8
Oscar <oscarjakobsen@icloud.com>
2025-11-26 16:22:37 +0100

    remade migrations

--- src/Chirp.Infrastructure/Migrations/20251126151715_InitialCreate.Designer.cs
+++ src/Chirp.Infrastructure/Migrations/20251126151715_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Infrastructure.Migrations
 {
     [DbContext(typeof(CheepDbContext))]
-    [Migration("20251126145329_InitialCreate")]
+    [Migration("20251126151715_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
