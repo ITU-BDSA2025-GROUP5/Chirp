9bccb33292ab6fc60fe72d7670ea77c475897fb7
Martin Jæger <Maraj@itu.dk>
2025-11-27 16:21:42 +0100

    Created a bunc of UI test using PlayWright

---
+++ Test/PlaywrightTests/LoggedInUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightTests;
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class LoggedInUI : PageTest
+{
+    [Test]
+    public async Task Login()
+    {
+        
+    }
+}
