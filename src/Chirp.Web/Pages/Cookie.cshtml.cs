f1ac6b4356d8e5f3925881910ad25578d5649d2a
nisse-bit <niej@itu.dk>
2025-11-28 15:51:39 +0100

    small changes

--- src/Chirp.Web/Pages/Cookie.cshtml.cs
+++ src/Chirp.Web/Pages/Cookie.cshtml.cs
@@ -1,11 +1,35 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
+// Services/CookieConsentService.cs
+using Microsoft.AspNetCore.Http;
 
-namespace Chirp.Razor.Pages;
-
-public class Cookie : PageModel
+public class CookieConsentService
 {
-    public void OnGet()
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    
+    public CookieConsentService(IHttpContextAccessor httpContextAccessor)
     {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
+    public bool HasConsent()
+    {
+        return _httpContextAccessor.HttpContext?.Request.Cookies["CookieConsent"] != null;
+    }
+    
+    public string? GetConsentStatus()
+    {
+        return _httpContextAccessor.HttpContext?.Request.Cookies["CookieConsent"];
+    }
+    
+    public void SetConsent(string status)
+    {
+        var options = new CookieOptions
+        {
+            Expires = DateTime.Now.AddYears(1),
+            HttpOnly = true,
+            Secure = false, // Set to true in production with HTTPS
+            SameSite = SameSiteMode.Lax
+        };
         
+        _httpContextAccessor.HttpContext?.Response.Cookies.Append("CookieConsent", status, options);
     }
 }
